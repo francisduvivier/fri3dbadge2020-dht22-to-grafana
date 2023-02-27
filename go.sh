f5934125383db3958652dd4968a3c199026ed9e0
Francis Duvivier <francisduvivier@gmail.com>
2023-02-27 09:16:37 +0800

    Add grafana and fix prometheus config.
    Fully working setup now. Only manual steps currenlty are creating the node-red flows and creating a dashboard in grafana.

--- go.sh
+++ go.sh
@@ -1,4 +1,8 @@
 mkdir /root/node-red-data -p 
 chmod 777 /root/node-red-data
+mkdir /root/prometheus_data -p
+chmod 777 /root/prometheus_data
+mkdir /root/grafana-data -p
+chmod 777 /root/grafana-data
 docker-compose up -d
 
